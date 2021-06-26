012b07f61af56cbc3a6663db2676a24385016566
Vishnu-prathap <85738017+Vishnu-prathap@users.noreply.github.com>
2021-06-26 17:35:56 +0530

    Update prog_1.m

--- Basics/prog_1.m
+++ Basics/prog_1.m
@@ -26,5 +26,28 @@ matB = [(5:5:30);(30:5:55);(55:5:80)]
 v = [(99:-1:91)]
 matA([1 3 4 5],3:6)=[matB([1 2],1:4);v(5:8);matB(3,2:5)]
 
+//6
+A = [1 2 3;-8 5 7;-8 4 6];
+B = [12 -5 4;7 11 6;1 8 13];
+C = [7 13 4;-2 8 -5;9 -6 11];
+
+%a
+d1 = A+B
+d2 = B+A
+% solutions of d1 and d2 will bw same
+%b
+s1 = A+(B+C)
+s2 = (A+B)+C
+% Solutions of s1 and s2 will be same
+%c
+r1 = 7*(A+C)
+r2 = 7*A +7*C
+% Solutions of r1 and r2 will be same
+%d
+t1=A*(B+C)
+t2=(A*B)+(A*C)
+% Solutiions of t1 and t2 will be same
+
+
 
   
